4d29054bcd08c84dbe2f22fb35d7ee91c478554f
Ramanan Rajeswaran <ramanan@google.com>
2012-08-31 13:41:41 -0700

    move grouper specific vars to device.mk
    Change-Id: I99ac60e01c6b03c83e0670d39de3ed35a1df9f3d
    
    Conflicts:
    
    	device-common.mk

--- device-common.mk
+++ device-common.mk
@@ -31,7 +31,6 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 include frameworks/native/build/tablet-7in-hdpi-1024-dalvik-heap.mk
 
 PRODUCT_COPY_FILES += \
-    device/asus/grouper/init.grouper.rc:root/init.grouper.rc \
     device/asus/grouper/fstab.grouper:root/fstab.grouper \
     device/asus/grouper/ueventd.grouper.rc:root/ueventd.grouper.rc \
     device/asus/grouper/init.grouper.usb.rc:root/init.grouper.usb.rc \
